4fb86b93ea9ca5d79d0fc96f686fe7aec4ca12d6
VSah7079 <vk3815155@gmail.com>
2025-08-20 22:24:44 +0530

    refactor: standardize code formatting and add mongoose dependency; implement database connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,18 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-
+const cors = require("cors");
+const connectToDb = require("./db/db");
+// Connect to database
+connectToDb();
+// Middleware
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-
 
 module.exports = app;
